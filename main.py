f8a6a8ce7093e78f01b502981efc8adbe86c67cb
David Xue <David.Xue37@myhunter.cuny.edu>
2023-04-23 17:39:31 -0400

    Add embed for SP/Unique skills for units

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ async def unit_search(interaction, char_name:str):
     result = mycursor.fetchall()
     #for i in result[0]:
     #    print(i,end='\n')
+    all_embeds = []
     character_embed = discord.Embed()
     character_embed.set_author(name=char_name)
     character_embed.add_field(name="UB",value = "\n\u200b",inline=False)
@@ -52,12 +53,22 @@ async def unit_search(interaction, char_name:str):
     character_embed.add_field(name=result[0][13],value = result[0][14],inline=False)
     character_embed.add_field(name="EX Skill",value = "\n\u200b",inline=False)
     character_embed.add_field(name=result[0][15],value=result[0][16],inline=False)
+    all_embeds.append(character_embed)
+    if(result[0][17]!="None" and result[0][18]!="None"):
+        special_unique_skills = discord.Embed()
+        special_unique_skills.set_author(name="Special/Unique/Hidden Skills")
+        special_unique_skills.add_field(name=result[0][17],value=result[0][18],inline=False)
+        if(result[0][19]!="None" and result[0][20]!="None"):
+            special_unique_skills.add_field(name=result[0][19],value=result[0][20],inline=False)
+        if(result[0][21]!="None" and result[0][22]!="None"):
+            special_unique_skills.add_field(name=result[0][21],value=result[0][22],inline=False)
+        if(result[0][23]!="None" and result[0][24]!="None"):
+            special_unique_skills.add_field(name=result[0][23],value=result[0][24],inline=False)
+        all_embeds.append(special_unique_skills)
     misc = discord.Embed()
     misc.set_author(name="MISC")
     misc.add_field(name="Initial movement: " ,value=result[0][-2],inline=False)
     misc.add_field(name="Loop pattern: ",value = result[0][-1],inline=False)
-    all_embeds = []
-    all_embeds.append(character_embed)
     all_embeds.append(misc)
     await interaction.response.send_message(embeds = [all_embeds[0]],view = ButtonMenu(all_embeds,60,interaction.user))
 
