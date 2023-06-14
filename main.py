e2478d39e3ab9ea27a6a9497a506658049181080
David Xue <David.Xue37@myhunter.cuny.edu>
2023-06-14 15:52:46 -0400

    Added icons for UB,Skill 1, Skill 2, and EX
    For Peco, Kyaru, Kokkoro, Shefi, Yui, Rei and Hiyori currently

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def unit_search(interaction, char_name:str):
     all_embeds = []
     character_embed = discord.Embed(description = "The Numbers shown below are when they're lvl "+ str(char_dict['level']) +".\nFor more information, Go [Here](https://docs.google.com/spreadsheets/d/1JjK7Ws4gfzKChRs5ueoxEZVN5SXK10nhDC1-nbm0NUs/edit#gid=1918232313)")
     character_embed.set_author(name=version_name)
-    character_embed.add_field(name="UB",value = "\n\u200b",inline=False)
+    character_embed.add_field(name=char_dict["UB_icon"] + " UB",value = "\n\u200b",inline=False)
     if(char_dict['6_star_icon']=="None"):
         character_embed.set_thumbnail(url=char_dict['3_star_icon'])
     else:
@@ -56,15 +56,15 @@ async def unit_search(interaction, char_name:str):
     else:
         character_embed.add_field(name=char_dict['UB_name']+'\n',value = char_dict['UB'],inline=True)
         character_embed.add_field(name="Unlock at 6⭐️: \n" + char_dict['UB+_name']+'\n',value = char_dict['UB+'],inline=True)
-    character_embed.add_field(name="Skill 1",value = "\n\u200b",inline=False)
+    character_embed.add_field(name=char_dict['Skill_1_icon'] + " Skill 1",value = "\n\u200b",inline=False)
     if(char_dict['Skill_1+_name']=="None" and char_dict['Skill_1+']=="None"):
         character_embed.add_field(name=char_dict['Skill_1_name'],value=char_dict['Skill_1'],inline=False)
     else:
         character_embed.add_field(name=char_dict['Skill_1_name']+'\n',value = char_dict['Skill_1'],inline=True)
         character_embed.add_field(name="Unlock after UE: \n" + char_dict['Skill_1+_name']+'\n',value = char_dict['Skill_1+'],inline=True)
-    character_embed.add_field(name="Skill 2",value = "\n\u200b",inline=False)
+    character_embed.add_field(name=char_dict['Skill_2_icon'] + " Skill 2",value = "\n\u200b",inline=False)
     character_embed.add_field(name=char_dict['Skill_2_name'],value = char_dict['Skill_2'],inline=False)
-    character_embed.add_field(name="EX Skill",value = "\n\u200b",inline=False)
+    character_embed.add_field(name=char_dict['EX_skill_icon'] + " EX Skill",value = "\n\u200b",inline=False)
     character_embed.add_field(name=char_dict['EX_skill_name'],value=char_dict['EX_skill'],inline=False)
     all_embeds.append(character_embed)
     if(char_dict['Special_Skills_1_Name']!="None" and char_dict['Special_Skills_1']!="None"):
